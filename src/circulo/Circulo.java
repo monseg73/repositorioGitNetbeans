56aab9ca236285da236ad0fcbca6bcd026a7d022
monseg73 <monsegcarretero@gmail.com>
2022-01-05 22:36:02 +0100

    Validación proyecto de Circulo

---
+++ src/circulo/Circulo.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package circulo;
+import java.util.Scanner;
+
+/**
+ *
+ * @author monse
+ */
+public class Circulo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        final double PI=3.1416;
+            // TODO Apéndice de método generado automáticamente
+		Scanner s=new Scanner(System.in);
+		System.out.println("Introduce el radio:");
+		double r=s.nextDouble();
+		double area=(PI*r*r);
+		System.out.printf("El valor del área es %f\n", area);
+               
+	
+    }
+    
+}
