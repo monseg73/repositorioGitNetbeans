739bcd3c6a5a8d8701df38f6e563cd37d8995ef5
monseg73 <monsegcarretero@gmail.com>
2022-01-05 22:57:56 +0100

    Validación modificación de Circulo

--- src/circulo/Circulo.java
+++ src/circulo/Circulo.java
@@ -19,12 +19,19 @@ public class Circulo {
         // TODO code application logic here
         final double PI=3.1416;
             // TODO Apéndice de método generado automáticamente
+            try
+            {
 		Scanner s=new Scanner(System.in);
 		System.out.println("Introduce el radio:");
 		double r=s.nextDouble();
 		double area=(PI*r*r);
 		System.out.printf("El valor del área es %f\n", area);
-               
+            }
+            catch (Exception e)
+            {
+                    System.out.println(e.getMessage());
+            }
+            
 	
     }
     
